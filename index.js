60cd447adcb95d13067f493961af997102ff03be
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-07-28 12:22:14 +0800

    GIT

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const expressLayouts = require("express-ejs-layouts");
+const PORT = process.env.PORT;
+const app = express();
+const router = require("./routes/api");
+const bookRouter = require("./routes/bookRouter");
+
+app.use(expressLayouts);
+app.set("layout", "./layouts/full-width");
+app.set("view engine", "ejs");
+app.use(router);
+app.use("/books", bookRouter);
+
+app.get("", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log("Running");
+});
